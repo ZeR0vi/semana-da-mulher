25d5c95c26444ece201b65024f293dec53021827
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-03-07 17:05:17 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route  path="/" element={  <Main/> }/>
-          <Route path="/projeto" element={ <Project/> } />
+          <Route path="projeto" element={ <Project/> } />
         </Routes>
       </BrowserRouter>
   )
