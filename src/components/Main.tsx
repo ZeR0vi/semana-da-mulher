e09f934e40fa3352abb0c399db5c689692576cd8
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-03-07 17:07:27 -0300

    Add files via upload

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -36,7 +36,7 @@ export  function Main(){
                                        sobre a escritora brasileira Clarice Lispector.
                                     </p>
 
-                                    <a href='/projeto'>CONHECER O PROJETO</a>
+                                    <a href='projeto'>CONHECER O PROJETO</a>
                               </div>
                         </div>
 
